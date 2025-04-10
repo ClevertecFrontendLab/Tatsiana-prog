dcd0f40c47d5b095f972b33b48b489a8fca6b075
Tatsiana-prog <programvtv07@gmail.com>
2025-04-10 13:11:33 +0300

    feat(userMetrics): add new block UserMetrics, add new icon

--- src/components/Pages/Home/Home.tsx
+++ src/components/Pages/Home/Home.tsx
@@ -3,29 +3,33 @@ import { Box, Flex, Heading } from '@chakra-ui/react';
 import { Header } from '../../Header/Header';
 import { SearchList } from '../../SearchList/SearchList';
 import Sidebar from '../../SideBar/SideBar';
+import { UserMetrics } from '../../UserMetrics/UserMetrics';
 
 const Home = () => (
     <>
         <Header></Header>
         <main>
-            <section>
-                <Flex justifyContent='space-between'>
+            <>
+                <Flex justifyContent='space-between' height='1040px'>
                     <Sidebar />
                     <Box>
-                        <Heading
-                            as='h1'
-                            size='48px'
-                            color='0000'
-                            mt='32px'
-                            mb='32px'
-                            fontWeight='bold'
-                        >
-                            Приятного аппетита!
-                        </Heading>
-                        <SearchList></SearchList>
+                        <section>
+                            <Heading
+                                as='h1'
+                                size='48px'
+                                color='0000'
+                                mt='32px'
+                                mb='32px'
+                                fontWeight='bold'
+                            >
+                                Приятного аппетита!
+                            </Heading>
+                            <SearchList></SearchList>
+                        </section>
                     </Box>
+                    <UserMetrics />
                 </Flex>
-            </section>
+            </>
         </main>
     </>
 );
---
+++ src/components/UserMetrics/UserMetrics.tsx
@@ -0,0 +1,46 @@
+import { Button, Flex } from '@chakra-ui/react';
+
+import ReciepeIcon from '../../assets/icons/icon-recipe.svg';
+import { CardStorage } from '../Metrics/CardStorage';
+import { FollowersCounter } from '../Metrics/FollowersCounter';
+import { LikeCounter } from '../Metrics/LikeCounter';
+
+export const UserMetrics = () => (
+    <Flex flexDirection='column' justifyContent='space-between' alignItems='center'>
+        <Flex flexDirection='column' gap='32px' pt='16px' pr='56px' pl='114px'>
+            <CardStorage />
+            <FollowersCounter />
+            <LikeCounter />
+        </Flex>
+        <Button
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                width: '208px',
+                height: '208px',
+                right: '0',
+                padding: '0',
+                background:
+                    'radial-gradient(ellipse 50.00% 50.00% at 50.00% 50.00%, rgba(196, 255, 97, 0.70) 0%, rgba(255, 255, 255, 0) 100%)',
+            }}
+        >
+            <img
+                src={ReciepeIcon}
+                alt='Save'
+                style={{
+                    marginBottom: '12px',
+                }}
+            />
+            <span
+                style={{
+                    fontSize: '12px',
+                    fontWeight: '400',
+                    color: 'rgba(0 , 0, 0, 0.64)',
+                }}
+            >
+                Записать рецепт
+            </span>
+        </Button>
+    </Flex>
+);
